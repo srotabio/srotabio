fecd3aa6ff57615055ce4997726989b58d9e3925
Abhirath <abhs.94@gmail.com>
2026-08-03 16:40:15 +0530

    changeS

--- site-config.js
+++ site-config.js
@@ -1,14 +1,3 @@
-/*
-  Add a static-form endpoint here before publishing.
-
-  Recommended lean setup:
-  1. Create a free Formspree form.
-  2. Copy its endpoint, for example https://formspree.io/f/abcdefgh
-  3. Paste it between the quotes below.
-
-  No other site file needs to change.
-*/
-
 window.SROTA_SITE_CONFIG = {
-  waitlistEndpoint: "",
+  waitlistEndpoint: "https://api.srotabio.com",
 };
